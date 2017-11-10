b10669fab993600e51b24c4444ecbe11ef67beaa
Toha <tohenk@yahoo.com>
2017-11-10 15:30:50 +0700

    Add Popperjs.
    Signed-off-by: Toha <tohenk@yahoo.com>

---
+++ src/NTLAB/JS/Script/Popper.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017 Toha <tohenk@yahoo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace NTLAB\JS\Script;
+
+use NTLAB\JS\Script as Base;
+use NTLAB\JS\Util\Asset;
+
+/**
+ * Popper.js javascript code repository for PHP.
+ *
+ * @author Toha
+ */
+class Popper extends Base
+{
+    const UMD = 'umd';
+    const ESM = 'esm';
+    const ESNext = '';
+
+    /**
+     * @var string
+     */
+    protected $repository = 'popper.js';
+
+    /**
+     * @var string
+     */
+    protected $version = self::UMD;
+
+    protected function configure()
+    {
+        $this->setVersion($this->version);
+        $this->addAsset(Asset::ASSET_JAVASCRIPT, 'popper.min');
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \NTLAB\JS\Script::getRepositoryName()
+     */
+    protected function getRepositoryName()
+    {
+        return $this->repository;
+    }
+
+    /**
+     * Set version type.
+     *
+     * @param string $version Popper version to use
+     * @return \NTLAB\JS\Script\Popper
+     */
+    public function setVersion($version)
+    {
+        switch ($version) {
+            case static::UMD:
+                $this->getAsset()->setPath(Asset::ASSET_JAVASCRIPT, static::UMD);
+                break;
+            case static::ESM:
+                $this->getAsset()->setPath(Asset::ASSET_JAVASCRIPT, static::ESM);
+                break;
+            case static::ESM:
+                $this->getAsset()->setPath(Asset::ASSET_JAVASCRIPT, static::ESNext);
+                break;
+        }
+
+        return $this;
+    }
+}
